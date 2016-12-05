c5507c70a894b995ada2ba5a7dde70de8939558b
Evan Hsu <evanhsu@gmail.com>
2016-12-04 22:50:01 -0700

    Added customizable banner images to the top of each page

--- storage/streams/src/models/Pages/PagesBlogPagesEntryModel.php
+++ storage/streams/src/models/Pages/PagesBlogPagesEntryModel.php
@@ -14,14 +14,18 @@ class PagesBlogPagesEntryModel extends EntryModel
     protected $titleName = 'id';
 
     protected $rules = [
+'banner_image' => '',
+'slogan_line_1' => '',
 ];
 
     protected $fields = [
+'banner_image',
+'slogan_line_1',
 ];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
-    protected $relationships = [];
+    protected $relationships = ['banner_image'];
 
     
 
@@ -44,6 +48,86 @@ class PagesBlogPagesEntryModel extends EntryModel
 'translatable' => '1',
 'config' => 'a:0:{}',
 'assignments' => [
+[
+'id' => '132',
+'sort_order' => '132',
+'stream_id' => '25',
+'field_id' => '121',
+'config' => 'a:1:{i:0;s:6:"a:0:{}";}',
+'unique' => '0',
+'required' => '0',
+'translatable' => '0',
+'field' => [
+'id' => '121',
+'namespace' => 'pages',
+'slug' => 'banner_image',
+'type' => 'anomaly.field_type.file',
+'config' => 'a:2:{s:7:"folders";a:1:{i:0;s:1:"1";}s:3:"max";s:3:"7.0";}',
+'locked' => '0',
+'translations' => [
+[
+'id' => '121',
+'field_id' => '121',
+'locale' => 'en',
+'name' => 'Banner Image',
+'placeholder' => '',
+'warning' => '',
+'instructions' => '',
+],
+],
+],
+'translations' => [
+[
+'id' => '132',
+'assignment_id' => '132',
+'locale' => 'en',
+'label' => '',
+'warning' => '',
+'placeholder' => '',
+'instructions' => '',
+],
+],
+],
+[
+'id' => '134',
+'sort_order' => '134',
+'stream_id' => '25',
+'field_id' => '116',
+'config' => 'a:1:{i:0;s:6:"a:0:{}";}',
+'unique' => '0',
+'required' => '0',
+'translatable' => '0',
+'field' => [
+'id' => '116',
+'namespace' => 'pages',
+'slug' => 'slogan_line_1',
+'type' => 'anomaly.field_type.text',
+'config' => 'a:6:{s:4:"type";s:4:"text";s:3:"min";N;s:3:"max";N;s:12:"show_counter";b:0;s:9:"suggested";s:2:"25";s:13:"default_value";N;}',
+'locked' => '0',
+'translations' => [
+[
+'id' => '116',
+'field_id' => '116',
+'locale' => 'en',
+'name' => 'Slogan (Line 1)',
+'placeholder' => '',
+'warning' => '',
+'instructions' => 'This will be displayed in a smaller, italic heading font on the home page.',
+],
+],
+],
+'translations' => [
+[
+'id' => '134',
+'assignment_id' => '134',
+'locale' => 'en',
+'label' => 'Slogan',
+'warning' => '',
+'placeholder' => '',
+'instructions' => 'This text will be overlayed on top of the banner image',
+],
+],
+],
 ],
 'translations' => [
 [
@@ -57,4 +141,10 @@ class PagesBlogPagesEntryModel extends EntryModel
 ];
 
     
+public function bannerImage()
+{
+
+return $this->getFieldType('banner_image')->getRelation();
+}
+
 }
--- storage/streams/src/models/Pages/PagesDefaultPagesEntryModel.php
+++ storage/streams/src/models/Pages/PagesDefaultPagesEntryModel.php
@@ -15,15 +15,19 @@ class PagesDefaultPagesEntryModel extends EntryModel
 
     protected $rules = [
 'content' => '',
+'banner_image' => '',
+'slogan_line_1' => '',
 ];
 
     protected $fields = [
 'content',
+'banner_image',
+'slogan_line_1',
 ];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
-    protected $relationships = [];
+    protected $relationships = ['banner_image'];
 
     protected $translatedAttributes = ['content'];
 
@@ -86,6 +90,86 @@ class PagesDefaultPagesEntryModel extends EntryModel
 ],
 ],
 ],
+[
+'id' => '133',
+'sort_order' => '133',
+'stream_id' => '23',
+'field_id' => '121',
+'config' => 'a:1:{i:0;s:6:"a:0:{}";}',
+'unique' => '0',
+'required' => '0',
+'translatable' => '0',
+'field' => [
+'id' => '121',
+'namespace' => 'pages',
+'slug' => 'banner_image',
+'type' => 'anomaly.field_type.file',
+'config' => 'a:2:{s:7:"folders";a:1:{i:0;s:1:"1";}s:3:"max";s:3:"7.0";}',
+'locked' => '0',
+'translations' => [
+[
+'id' => '121',
+'field_id' => '121',
+'locale' => 'en',
+'name' => 'Banner Image',
+'placeholder' => '',
+'warning' => '',
+'instructions' => '',
+],
+],
+],
+'translations' => [
+[
+'id' => '133',
+'assignment_id' => '133',
+'locale' => 'en',
+'label' => '',
+'warning' => '',
+'placeholder' => '',
+'instructions' => '',
+],
+],
+],
+[
+'id' => '135',
+'sort_order' => '135',
+'stream_id' => '23',
+'field_id' => '116',
+'config' => 'a:1:{i:0;s:6:"a:0:{}";}',
+'unique' => '0',
+'required' => '0',
+'translatable' => '0',
+'field' => [
+'id' => '116',
+'namespace' => 'pages',
+'slug' => 'slogan_line_1',
+'type' => 'anomaly.field_type.text',
+'config' => 'a:6:{s:4:"type";s:4:"text";s:3:"min";N;s:3:"max";N;s:12:"show_counter";b:0;s:9:"suggested";s:2:"25";s:13:"default_value";N;}',
+'locked' => '0',
+'translations' => [
+[
+'id' => '116',
+'field_id' => '116',
+'locale' => 'en',
+'name' => 'Slogan (Line 1)',
+'placeholder' => '',
+'warning' => '',
+'instructions' => 'This will be displayed in a smaller, italic heading font on the home page.',
+],
+],
+],
+'translations' => [
+[
+'id' => '135',
+'assignment_id' => '135',
+'locale' => 'en',
+'label' => '',
+'warning' => '',
+'placeholder' => '',
+'instructions' => '',
+],
+],
+],
 ],
 'translations' => [
 [
@@ -99,4 +183,10 @@ class PagesDefaultPagesEntryModel extends EntryModel
 ];
 
     
+public function bannerImage()
+{
+
+return $this->getFieldType('banner_image')->getRelation();
+}
+
 }
